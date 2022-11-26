68508118bb0b85f823c0f16c3c9af231b03d2363
Eugenio <eugeniolgimenez@gmail.com>
2022-11-26 08:30:13 -0300

    port

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ import { firestore, rtdb } from "./db"; //importo la DB
 import * as express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); //parsea el body que le enviamos (req.body)
 
